ca81a90a2c770fe9aadd452aa33dceb213f3a064
Micheal <mlnguyen@ualberta.ca>
2018-01-31 12:47:42 -0700

    Second commit

--- app/src/main/java/com/example/mlnguyen_subbook/SubscriptionEditActivity.java
+++ app/src/main/java/com/example/mlnguyen_subbook/SubscriptionEditActivity.java
@@ -2,6 +2,7 @@ package com.example.mlnguyen_subbook;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  * Created by kio22 on 2018-01-30.
@@ -13,5 +14,7 @@ public class SubscriptionEditActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subscriptionedit);
     }
-
+    public void onCancel(View view){
+        finish();
+    }
 }
